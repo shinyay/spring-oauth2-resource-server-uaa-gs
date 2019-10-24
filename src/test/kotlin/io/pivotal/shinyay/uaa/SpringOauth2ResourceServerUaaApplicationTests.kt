a2b4fa2603d841dfd46d3bdb17972fbe6939cf9c
shinyay <shinya.com@gmail.com>
2019-10-24 21:43:39 +0900

    Initial Commit

---
+++ src/test/kotlin/io/pivotal/shinyay/uaa/SpringOauth2ResourceServerUaaApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.pivotal.shinyay.uaa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringOauth2ResourceServerUaaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
