5645c0d6a43581e45efdee26030db575f45d6e50
shinyay <shinya.com@gmail.com>
2019-10-26 20:42:49 +0900

    Add WebSecurityConfigurerAdapter as super class

--- src/main/kotlin/io/pivotal/shinyay/uaa/configuration/OAuth2ResourceServerConfiguration.kt
+++ src/main/kotlin/io/pivotal/shinyay/uaa/configuration/OAuth2ResourceServerConfiguration.kt
@@ -1,7 +1,12 @@
 package io.pivotal.shinyay.uaa.configuration
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
 @EnableWebSecurity
-class OAuth2ResourceServerConfiguration {
+class OAuth2ResourceServerConfiguration : WebSecurityConfigurerAdapter() {
+    override fun configure(http: HttpSecurity?) {
+        super.configure(http)
+    }
 }
