37801772d48172c8fd044420758ea5ec612a40ac
shinyay <shinya.com@gmail.com>
2019-10-26 20:34:37 +0900

    Add Spring Security Enabled annotation

---
+++ src/main/kotlin/io/pivotal/shinyay/uaa/configuration/OAuth2ResourceServerConfiguration.kt
@@ -0,0 +1,7 @@
+package io.pivotal.shinyay.uaa.configuration
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+
+@EnableWebSecurity
+class OAuth2ResourceServerConfiguration {
+}
